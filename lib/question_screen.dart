453e70bdccfa0a99f5fad020548d126e22732aad
Your Name <you@example.com>
2024-04-12 21:07:15 +0100

    display answer and question

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/answers_button.dart';
+import 'package:adv_basics/data/question.dart';
 import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -13,25 +14,34 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final displayedQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'This is the question screen',
-            style: TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              displayedQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          AnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {},
-          )
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...displayedQuestion.answers
+                .map((answer) => AnswerButton(onTap: () {}, answerText: answer))
+            // AnswerButton(
+            //   answerText: 'Answer 1',
+            //   onTap:,
+            // )
+          ],
+        ),
       ),
     );
   }
